e7d1cb285d443391b5eee8885cd0ea141c77fd65
kyawmyotun <mg.kmt.kyawmyotun@gmail.com>
2021-06-23 10:24:19 +0630

    fix mout

--- app/snowpack.config.mjs
+++ app/snowpack.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
-    public: { url: '/', static: true },
-    src: { url: '/dist' },
+    public: { url: '/app', static: true },
+    src: { url: '/app/dist' },
   },
   plugins: [
     '@snowpack/plugin-react-refresh',
@@ -17,7 +17,7 @@ export default {
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    { match: 'routes', src: '.*', dest: '/app/index.html' },
   ],
   optimize: {
     /* Example: Bundle your final build: */
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -5,6 +5,9 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <Router>
+      <Route path="/" component={App}></Route>
+    </Router> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
