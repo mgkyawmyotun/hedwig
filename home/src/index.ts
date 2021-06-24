5970ec018a8ae5a8ccb2366d792a4ba9d6c0ad6e
kyawmyotun <mg.kmt.kyawmyotun@gmail.com>
2021-06-24 18:34:12 +0630

    :white_check_mark: completed mobile design

--- home/src/index.ts
+++ home/src/index.ts
@@ -1,2 +1,20 @@
 /* Add JavaScript code here! */
-console.log('Hello World! You did it! Welcome to Snowpack :D');
+const hidden_menu_items = document.querySelector('.menu + div');
+const menu = document.querySelector('.menu');
+menu.addEventListener('click', () => {
+  if (hidden_menu_items.classList.contains('hidden')) {
+    hidden_menu_items.classList.toggle('hidden', false);
+    return;
+  }
+  hidden_menu_items.classList.toggle('hidden', true);
+});
+
+hidden_menu_items.childNodes.forEach((li) => {
+  li.addEventListener('click', () => {
+    if (hidden_menu_items.classList.contains('hidden')) {
+      hidden_menu_items.classList.toggle('hidden', false);
+      return;
+    }
+    hidden_menu_items.classList.toggle('hidden', true);
+  });
+});
