74f38d32921e73262294236dc36b9b340f926eff
kyawmyotun <mg.kmt.kyawmyotun@gmail.com>
2021-06-25 11:22:37 +0630

    :sparkles: add desktop layout for hero/nav

--- home/src/index.ts
+++ home/src/index.ts
@@ -9,8 +9,13 @@ menu.addEventListener('click', () => {
   hidden_menu_items.classList.toggle('hidden', true);
 });
 
-hidden_menu_items.childNodes.forEach((li) => {
+hidden_menu_items.childNodes.forEach((li: Element) => {
   li.addEventListener('click', () => {
+    const prevactive_element = hidden_menu_items.querySelector('.active');
+    if (prevactive_element)
+      prevactive_element.classList.toggle('active', false);
+    li.classList.toggle('active', true);
+
     if (hidden_menu_items.classList.contains('hidden')) {
       hidden_menu_items.classList.toggle('hidden', false);
       return;
