e89fb3f77276aee451c4c6a4ba1a2a7dd6c26685
kyawmyotun <mg.kmt.kyawmyotun@gmail.com>
2021-07-01 14:01:48 +0630

    :package: exclude build file

--- build/app/dist/App.js
+++ build/app/dist/App.js
@@ -1,12 +1,10 @@
 import React from "../_snowpack/pkg/react.js";
-import "./App.css.proxy.js";
+import {Main} from "./components/main/index.js";
+import {NavBar} from "./components/NavBar.js";
 import "./scss/index.css.proxy.js";
 function App({}) {
   return /* @__PURE__ */ React.createElement("div", {
     className: "App"
-  }, /* @__PURE__ */ React.createElement("h1", null, "Hello From App"), /* @__PURE__ */ React.createElement("img", {
-    src: "/app/assets/HedgwigLogoLight.png",
-    alt: ""
-  }));
+  }, /* @__PURE__ */ React.createElement(NavBar, null), /* @__PURE__ */ React.createElement(Main, null));
 }
 export default App;
--- build/app/dist/scss/index.css.proxy.js
+++ build/app/dist/scss/index.css.proxy.js
@@ -1,6 +1,6 @@
 // [snowpack] add styles to the page (skip if no document exists)
 if (typeof document !== 'undefined') {
-  const code = "body{background-color:#e2d2d2}";
+  const code = "body{--cultured: #f8f9faff;--cultured-2: #e9ecefff;--gainsboro: #dee2e6ff;--light-gray: #ced4daff;--cadet-blue-crayola: #adb5bdff;--slate-gray: #6c757dff;--davys-grey: #495057ff;--gunmetal: #343a40ff;--charleston-green: #212529ff}*,::after,::before{box-sizing:border-box}body{margin:0;background-color:var(--cultured)}h1{margin:0}p{margin:0}";
 
   const styleEl = document.createElement("style");
   const codeEl = document.createTextNode(code);
